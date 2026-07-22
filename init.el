6f48fb8f4a501975fad7636a13f879b26ed0d8b4
Robin Marchart <robin.marchart@gmail.com>
2026-07-22 20:49:05 +0200

    disable isa for now

--- init.el
+++ init.el
@@ -113,7 +113,7 @@
  upload                               ; map local to remote projects via ssh/ftp
 
  :lang
- isabelle
+ ;;isabelle
  ;;agda              ; types of types of types of types...
  (cc +lsp)                              ; C/C++/Obj-C madness
  ;;clojure           ; java with a lisp
